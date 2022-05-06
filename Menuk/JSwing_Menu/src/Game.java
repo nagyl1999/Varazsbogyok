06a5cb0740c821db2ddbaad91256c55f7b3621db
sudo-nkristof08 <bobphysics8@gmail.com>
2022-05-06 19:17:53 +0200

    Képernyők
    Képernyők layout-al

---
+++ Menuk/JSwing_Menu/src/Game.java
@@ -0,0 +1,142 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Insets;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Game {
+	
+	
+	private static JFrame frame;
+	private JPanel mainPanel, mapPanel, rightPanel, rightTopPanel ,inventoryPanel, agentsPanel, virologistPanel ,actionsPanel;
+	
+	
+	public ImageIcon getIcon(String Path) {
+		
+		ImageIcon img = new ImageIcon(Path);
+		Image image = img.getImage();
+		Image newimg = image.getScaledInstance(40, 40,  java.awt.Image.SCALE_SMOOTH);
+        img = new ImageIcon(newimg);
+		return img;
+	}
+	
+	public Game() {
+		
+		System.out.println("sd");
+		
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Game");
+		frame.setSize(800,800);
+		
+		JPanel mainPanel = new JPanel();
+		
+		mainPanel.setLayout(new GridLayout(1,0));
+		
+		JPanel mapPanel = new JPanel();
+        JPanel rightPanel = new JPanel(new GridLayout(0,1));
+        
+        JPanel inventoryPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        inventoryPanel.setBackground(Color.RED);
+        JPanel rightTopPanel = new JPanel();
+        JPanel actionsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        actionsPanel.setBackground(Color.cyan);
+        
+        JButton vir1, bot1, bot2, agens;
+        
+        vir1 = new JButton(getIcon("vir.PNG"));
+        
+        bot1 = new JButton(getIcon("bot.PNG"));
+        bot2 = new JButton(getIcon("bot.PNG"));
+        agens = new JButton(getIcon("teddy-bear.png")); 
+        
+        rightTopPanel.setLayout(new GridLayout(0,1));
+        
+        
+        virologistPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        agentsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        //virologistPanel.setLayout(new GridLayout(1,0));
+        virologistPanel.setBackground(Color.YELLOW);
+        agentsPanel.setBackground(Color.GREEN);
+        //agentsPanel.setLayout(new GridLayout(1,0));
+        
+        virologistPanel.add(vir1);
+        virologistPanel.add(bot1);
+        virologistPanel.add(bot2);
+        agentsPanel.add(agens);
+        
+        rightTopPanel.add(virologistPanel);
+        rightTopPanel.add(agentsPanel);
+        
+        rightPanel.add(rightTopPanel);
+        rightPanel.add(inventoryPanel);
+        rightPanel.add(actionsPanel);
+        
+        JButton glove, jacket, bag, axe;
+        glove = new JButton(getIcon("glove.PNG"));
+        jacket = new JButton(getIcon("jacket.PNG"));
+        bag = new JButton(getIcon("bag.PNG"));
+        axe = new JButton(getIcon("axe.PNG"));
+        
+        inventoryPanel.add(glove);
+        inventoryPanel.add(jacket);
+        inventoryPanel.add(bag);
+        inventoryPanel.add(axe);
+        
+        
+        JButton move, vaccine, rob, craft, place;
+
+        move = new JButton(getIcon("move.PNG"));       
+        vaccine = new JButton(getIcon("vaccine.PNG"));
+        rob = new JButton(getIcon("rob.PNG"));
+        craft = new JButton(getIcon("craft.PNG"));
+        place = new JButton(getIcon("place.PNG"));
+        
+        /*
+        actionsPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        
+		c.insets = new Insets(5, 5, 5, 5);
+        
+		c.gridx = 1;
+	    c.gridy = 0;
+        actionsPanel.add(move, c);
+        c.gridx = 2;
+	    c.gridy = 0;
+        actionsPanel.add(vaccine, c);
+        c.gridx = 3;
+	    c.gridy = 0;
+        actionsPanel.add(rob, c);
+        c.gridx = 4;
+	    c.gridy = 0;
+        actionsPanel.add(craft, c);
+        c.gridx = 5;
+	    c.gridy = 0;
+        actionsPanel.add(place, c);
+        */
+        
+        actionsPanel.add(move);
+        actionsPanel.add(vaccine);
+        actionsPanel.add(rob);
+        actionsPanel.add(craft);
+        actionsPanel.add(place);
+        
+        
+        mainPanel.add(mapPanel);
+        mainPanel.add(rightPanel);
+        
+        frame.add(mainPanel);
+        frame.setVisible(true);
+		
+	}
+	
+}
---
+++ Menuk/JSwing_Menu/src/Main.java
@@ -0,0 +1,192 @@
+
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpringLayout;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+
+public class Main {
+	
+	private static JFrame frame, frame2;
+	
+	public static void main(String args[]){
+		
+		
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Virológus");
+		frame.setSize(700,800);
+		
+		JPanel topText = new JPanel();
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        
+        ImageIcon img = new ImageIcon("kep.png");
+        
+        JLabel title = new JLabel("<html> <div style='text-align:center;'> VIROLÓGUS <br> BUMBÓ </div></html>", JLabel.LEFT);
+		title.setFont(new Font("Comic Sans MS", Font.BOLD, 30));
+		title.setForeground(Color.black);
+		JLabel kep = new JLabel("", img, JLabel.CENTER);
+		topText.add(title);
+		topText.add(kep);
+		
+		
+        // Define new buttons with different regions
+        JButton newGameBtn = new JButton("NEW GAME");
+        newGameBtn.setFont(new Font("Comic Sans MS", Font.BOLD, 25));
+        newGameBtn.setForeground(Color.black);
+        JButton loadGameBtn = new JButton("LOAD GAME");
+        loadGameBtn.setFont(new Font("Comic Sans MS", Font.BOLD, 25));
+        loadGameBtn.setForeground(Color.black);
+        JButton exitBtn = new JButton("EXIT"); 
+        exitBtn.setFont(new Font("Comic Sans MS", Font.BOLD, 25));
+        exitBtn.setForeground(Color.black);
+        
+       
+        c.fill = GridBagConstraints.HORIZONTAL;  
+		c.ipady = 30;
+		c.insets = new Insets(20, 10, 20, 10);
+				
+		c.gridx = 0;
+	    c.gridy = 1;
+	    panel.add(newGameBtn,c);
+		 
+		c.gridx = 0;  
+	    c.gridy = 2;
+	    panel.add(loadGameBtn,c);
+		
+		c.gridx = 0;  
+	    c.gridy = 3;
+	    panel.add(exitBtn,c);
+        
+	    panel.setBackground(new Color(255, 0, 82));
+	    topText.setBackground(new Color(255, 0, 82));
+	    
+	    newGameBtn.setBackground(new Color(255, 0, 82));
+	    newGameBtn.setBorder(BorderFactory.createLineBorder(new Color(191, 255, 0), 10));
+	    newGameBtn.addActionListener( new ActionListener()
+	    {
+	        @Override
+	        public void actionPerformed(ActionEvent e)
+	        {
+	        	frame2 = new JFrame();
+	    		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    		frame2.setTitle("Virológus PopUp");
+	    		frame2.setSize(300,500);
+	    		
+	    		JButton ok, cancel;
+	    		JLabel tile, bot;
+	    		JSpinner tileTF, botTF;
+	    		JPanel panel = new JPanel();
+	    		panel.setLayout(new GridBagLayout());
+	    		
+	    		tile = new JLabel("<html> <div style='text-align:center;'> Hány mezõ legyen <br> a pályán: </div></html>", SwingConstants.CENTER);
+	    		tile.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+	    		tileTF = new JSpinner();
+	    		
+	    		bot = new JLabel("<html>Hány Bot legyen: </html>", SwingConstants.CENTER);
+	    		bot.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+	    		botTF = new JSpinner();
+	    		
+	    		ok = new JButton("Ok");
+	    		ok.setFont(new Font("Comic Sans MS", Font.BOLD, 25));
+	    		ok.addActionListener( new ActionListener()
+	    	    {
+	    	        @Override
+	    	        public void actionPerformed(ActionEvent e)
+	    	        {
+	    	        	Game game = new Game();
+	    	        }
+	    	    });
+	    		
+	    		cancel = new JButton("Cancel");
+	    		cancel.setFont(new Font("Comic Sans MS", Font.BOLD, 25));
+	    		
+	    		cancel.addActionListener( new ActionListener()
+	    	    {
+	    	        @Override
+	    	        public void actionPerformed(ActionEvent e)
+	    	        {
+	    	        	frame2.setVisible(false);
+	    	        	frame.setVisible(true);
+	    	        }
+	    	    });
+	    		
+	    		c.fill = GridBagConstraints.HORIZONTAL;
+	    		c.ipady = 30;
+	    		c.insets = new Insets(10, 1, 10, 40);
+	    		
+	    		c.gridx = 0;
+	    	    c.gridy = 0;
+	    	    panel.add(tile,c);
+	    		 
+	    		c.gridx = 1;  
+	    	    c.gridy = 0;
+	    	    panel.add(tileTF,c);
+	    		
+	    	    c.gridx = 0;
+	    	    c.gridy = 1;
+	    	    panel.add(bot,c);
+	    		 
+	    		c.gridx = 1;  
+	    	    c.gridy = 1;
+	    	    panel.add(botTF,c);
+	    	    
+	    		c.gridx = 0;  
+	    	    c.gridy = 2;
+	    	    panel.add(ok,c);
+	    	    
+	    	    c.gridx = 1;  
+	    	    c.gridy = 2;
+	    	    panel.add(cancel,c);
+	    		
+	    	    panel.setBackground(Color.pink);
+	    	    frame2.add(panel);
+	    		
+	    		frame.setVisible(false);
+	    		frame2.setVisible(true);
+	        }
+	    });
+	    
+	    
+	    loadGameBtn.setBackground(new Color(255, 0, 82));
+	    loadGameBtn.setBorder(BorderFactory.createLineBorder(new Color(191, 255, 0), 10));
+	    
+	    exitBtn.setBackground(new Color(255, 0, 82));
+	    exitBtn.setBorder(BorderFactory.createLineBorder(new Color(191, 255, 0), 10));
+	    exitBtn.addActionListener( new ActionListener()
+	    {
+	        @Override
+	        public void actionPerformed(ActionEvent e)
+	        {
+	            System.exit(0);
+	        }
+	    });
+	    
+	    frame.add(topText, BorderLayout.NORTH);
+	    frame.add(panel);
+        frame.setVisible(true);
+		
+	}
+	
+}
