ed1e9ecc26f841451d824458963da4422e70a762
nagyl1999 <45666481+nagyl1999@users.noreply.github.com>
2022-05-09 12:16:05 +0200

    map panel added

--- Szkeleton/src/graphics/VarazsbogyokFrame.java
+++ Szkeleton/src/graphics/VarazsbogyokFrame.java
@@ -6,5 +6,23 @@ import javax.swing.*;
  * Megjeleníthető ablak
  */
 public class VarazsbogyokFrame extends JFrame {
+    /**
+     * Map panel
+     */
+    JPanel map;
+
+    /**
+     * Konstruktor
+     */
+    public VarazsbogyokFrame() {
+        init();
+    }
+
+    /**
+     * Komponsensek létrehozása
+     */
+    public void init() {
+        map = new JPanel();
+    }
 
 }
