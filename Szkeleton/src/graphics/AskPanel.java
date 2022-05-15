c9e2729be781337126412f4658fc5474b014db27
Eszter <95177547+EszterIbolya@users.noreply.github.com>
2022-05-15 10:34:30 +0200

    légyszi

--- Szkeleton/src/graphics/AskPanel.java
+++ Szkeleton/src/graphics/AskPanel.java
@@ -32,9 +32,9 @@ public class AskPanel extends JPanel {
         JLabel tile, bot;
         JSpinner tileTF, botTF;
         setPreferredSize(new Dimension(1000, 800));
-        
 
-        tile = new JLabel("<html> <div style='text-align:center;'> Number of Fields : </div></html>", SwingConstants.CENTER);
+
+        tile = new JLabel("<html> <div style='text-align:center;'> Hány mezõ legyen <br> a pályán: </div></html>", SwingConstants.CENTER);
         tile.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
         SpinnerModel model = new SpinnerNumberModel(100, Game.minTileCount, 1000, 1);
         tileTF = new JSpinner(model);
